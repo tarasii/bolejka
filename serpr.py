ec77e5ffcd756dc272f0adbf3c1839e209526d62
tarasii <tarasii@gmail.com>
2015-01-14 13:54:04 +0200

    serial read - write to console

---
+++ serpr.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#
+
+import mysql.connector
+import datetime
+import serial
+from time import sleep
+
+def getserial():
+  res = "0";
+  ser = serial.Serial(0, 9600)
+  try:
+    ser.open()
+    if (ser.isOpen()):
+      ser.write("a")
+      sleep(0.1)
+      line = ser.readline()
+      ser.close()
+
+  except:
+    print ("Error comm")
+
+  return res
+
+
+
+if __name__ == "__main__":
+
+  while (1=1):
+     val = getserial() 
+     print val
+     sleep(1)
+     

2d87b08361c8bea7b58aa4878b11f6858ad88d64
tarasii <tarasii@gmail.com>
2015-01-14 10:55:56 +0200

    Update ard.ino

--- ard.ino
+++ ard.ino
@@ -3,23 +3,30 @@
 #include "EmonLib.h"                   // Include Emon Library
 EnergyMonitor emon1;                   // Create an instance
 int incomingByte;
+int val;
 
 void setup()
 {  
   Serial.begin(9600);
   
-  emon1.current(1, 111.1);             // Current: input pin, calibration.
+  //emon1.current(1, 111.1);             // Current: input pin, calibration.
 }
 
 void loop()
 {
-  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  //double Irms = emon1.calcIrms(1480);  // Calculate Irms only
   
   //Serial.print(Irms*230.0);	       // Apparent power
   //Serial.print(" ");
+  
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
-    Serial.println(Irms);		       // Irms
+    
+    //Serial.println(Irms);		       // Irms
+    
+    val = analogRead(1);     // считываем значение
+    Serial.println(val);             // выводим полученное значение
+    
   }
 }
